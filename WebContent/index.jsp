ce4251bc12763e62e88e654dcf8e6860152e46db
virendrasinghrp <virendrasinghrp@gmail.com>
2021-06-06 18:46:55 +0530

    Initial commit

---
+++ WebContent/index.jsp
@@ -0,0 +1,16 @@
+
+<!-- Boilerplate. -->
+<jsp:root xmlns:jsp="http://java.sun.com/JSP/Page"
+          xmlns:c="http://java.sun.com/jsp/jstl/core"
+          version="2.0">
+<jsp:directive.page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" />
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+	<jsp:forward page="/DashboardServlet"></jsp:forward>
+	
+</body>
+</html>
+</jsp:root>
---
+++ src/dashboard/DashboardServlet.java
@@ -0,0 +1,45 @@
+package dashboard;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DashboardServlet
+ */
+@WebServlet(
+		urlPatterns = { "/DashboardServlet" }, 
+		initParams = { 
+				@WebInitParam(name = "name", value = "DashboardServlet")
+		})
+public class DashboardServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DashboardServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("pages/jsp/dashboard.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
